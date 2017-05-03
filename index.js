c05eeac9834f77ea881a88ec3966258c9944027b
Graeme Lockley <graemel@no9.co.za>
2017-05-03 21:15:35 +0200

    chore: ignore .idea/*

---
+++ index.js
@@ -0,0 +1,9 @@
+//- The `Bounded` interface for supports 2 functions which return the minimum and maximum value of the associated class.
+//-
+//- ```haskell
+//- interface Bounded a
+//-     minBound, maxBound :: () -> a
+//- ```
+
+module.exports = {
+};
